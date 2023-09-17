574e31b051df4f14c95e197e1bb270f7aa92582d
abdurrahim220 <abdurrahim5@gmail.com>
2023-09-18 02:10:02 +0600

    all css root component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-
 import './App.css'
-
+import About from './component/About/About'
+import Blog from './component/Blog/Blog'
+import Contact from './component/Contact/Contact'
+import Home from './component/Home/Home'
+import Portfolio from './component/Portfolio/Portfolio'
+import Pricing from './component/Pricing/Pricing'
+import Resume from './component/Resume/Resume'
+import Services from './component/Services/Services'
+import Sidebar from './component/Sidebar/Sidebar'
+import Testimonials from './component/Testimonials/Testimonials'
 function App() {
   
 
   return (
     <>
+    <Sidebar/>
+    <div className="main">
+      <Home/>
+      <About/>
+      <Services/>
+      <Resume/>
+      <Portfolio/>
+      <Blog/>
+      <Pricing/>
+      <Testimonials/>
+      <Contact/>
+    </div>
       
     </>
   )
